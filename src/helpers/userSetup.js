69b81f28a76e86f4f7f1981e02757f64148b6303
Philipp Flenker <philipp.flenker@zalando.de>
2025-05-01 09:30:00 +0200

    Fix feed

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -5,6 +5,8 @@ function userMarkdownSetup(md) {
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+  
+
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
