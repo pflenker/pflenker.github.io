1f95cbb360aeedf16779f460e1eb295b0f3de3c8
Philipp Flenker <philipp.flenker@zalando.de>
2025-05-06 20:02:47 +0200

    Remove jsdom as it's no longer needed

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,4 +1,3 @@
-const { JSDOM } = require("jsdom");
 function userMarkdownSetup(md) {
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
