f61bb4cbe4883c5a94752a36212517471f4a9959
Philipp Flenker <philipp.flenker@zalando.de>
2025-05-10 11:51:33 +0200

    Change Layout

--- src/helpers/userUtils.js
+++ src/helpers/userUtils.js
@@ -1,13 +1,46 @@
 const { DateTime } = require('luxon'); 
 
 function userComputed(data) {
+  let tags = [];
+  let type = "OTHER";
+  if (data.tags && data.tags.length > 0) {
+    tags = data.tags.filter(tag => tag !== "gardenEntry" && tag !== "note" && !tag.startsWith("dg"));
+    if (data.tags.includes("dgarticle")){
+      type = "ARTICLE";
+    } else if (data.tags.includes("dgblip")){
+      type = "BLIP"; 
+    } else if (data.tags.includes("dgstandalone")){
+      type = "STANDALONE";
+    }
+
+  }
+  
+  let created = data["created-date"] && DateTime.fromISO(data["created-date"], { zone: "Europe/Berlin" }).toJSDate();
+  let updated = data["updated-date"] && DateTime.fromISO(data["updated-date"], { zone: "Europe/Berlin" }).toJSDate();
+  let builtAt = new Date();
   return {
     author: "Philipp Flenker",
     email: "hello@philippflenker.com",
     description: "thoughts on stuff, views on things",
-    builtAt: new Date(),
-    created: DateTime.fromISO(data["created-date"], { zone: "Europe/Berlin" }).toJSDate(),
-    updated: DateTime.fromISO(data["updated-date"], { zone: "Europe/Berlin" }).toJSDate()
+    tags,
+    layout: {
+      nav: {
+        show: !["STANDALONE"].includes(type),
+      },
+      header: {
+        title: !["BLIP"].includes(type),
+        dates: created && updated && ["ARTICLE"].includes(type) && !["STANDALONE"].includes(type),
+        tags: tags.length > 0 && ["ARTICLE"].includes(type) && !["STANDALONE"].includes(type),
+      },
+      footer: {
+        show: !["STANDALONE"].includes(type),
+        builtat: builtAt && !data["hide-layout-footer-builtat"],
+        pageupdate: created && updated && !data["hide-layout-footer-pageupdate"] && !["BLIP", "ARTICLE"].includes(type),
+      }      
+    },
+    builtAt,
+    created,
+    updated
   };
 }
 
